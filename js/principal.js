7ec4571808a5c498fe55d1cd90ac7d524f9ad131
adrielli de moraes pedroso <106260133+adriimoraes@users.noreply.github.com>
2022-11-16 12:26:20 -0300

    Update principal.js

--- js/principal.js
+++ js/principal.js
@@ -20,18 +20,21 @@ for (var i = 0; i < pacientes.length; i++) {
     var pesoEhValido = true;
     var alturaEhValida = true;
 
-    if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
-    }
+    if(peso <= 0 || peso >= 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
 
-    if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida!");
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
-       
-    }
+    paciente.classList.add("paciente-invalido");
+}
+
+     if(altura <= 0 || altura >= 3.00){
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+
+    paciente.classList.add("paciente-invalido");
+}
 
 	
 

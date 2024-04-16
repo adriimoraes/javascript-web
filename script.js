3fc45844a046afdb4c47ae8f45a050e4a4afa206
adrielli de moraes pedroso <106260133+adriimoraes@users.noreply.github.com>
2024-04-16 12:16:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ document.getElementById('login-form').addEventListener('submit', function(event)
     console.log('Username:', username);
     console.log('Password:', password);
   });
+
+
+
+
+  document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+    var loginContainer = document.querySelector('.login-container');
+
+    // Verificar as credenciais
+    if (username === 'Pedro' && password === '12345678') {
+        // Credenciais corretas, mudar cor para verde
+        loginContainer.style.backgroundColor = '#4CAF50'; // verde
+    } else {
+        // Credenciais incorretas, mudar cor para vermelho
+        loginContainer.style.backgroundColor = '#f44336'; // vermelho
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
